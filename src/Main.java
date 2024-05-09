379bf315c07e4d2369f49fb26ef940003e447455
jeromechinjs <behnc.natural@gmail.com>
2024-05-09 23:37:13 +0800

    main program updated

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 import gui.TicketSystemGUI;
-import util.Initializer;
 
 public class Main {
     public static void main(String[] args) {
-        // Your existing initialization code
-        Initializer init = new Initializer();
         // Assuming TicketSystemGUI handles everything including initialization
         TicketSystemGUI gui = new TicketSystemGUI();
         gui.setVisible(true);
